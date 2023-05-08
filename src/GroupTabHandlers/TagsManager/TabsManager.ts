2780ec908a0e6ae878c64dbec7acef74ee6d3263
Madrigal11 <kurupgokul11@gmail.com>
2023-05-08 16:06:43 +0530

    feat(tab-manager): add-tab-to-existing-tab-group

--- src/GroupTabHandlers/TagsManager/TabsManager.ts
+++ src/GroupTabHandlers/TagsManager/TabsManager.ts
@@ -11,9 +11,13 @@ export interface ITabGroup {
   id: number
   title: string
 }
+
+export interface IMoveTabToGroupInput {
+  groupId: number
+  tabs: chrome.tabs.Tab[]
+}
 export class TabsManager {
   getCurrentTabQuery = { active: true, currentWindow: true }
-
   public async checkTabGroupExists({ tabGroupName }: IAddToGroupInput): Promise<boolean> {
     const groups: any = await new Promise((resolve) => {
       chrome.tabGroups.query({ title: tabGroupName }, (groups) => {
@@ -29,9 +33,27 @@ export class TabsManager {
       chrome.tabs.query(queryInfo, (tabs) => resolve(tabs))
     })
   }
-  public async groupTabs(tabs: chrome.tabs.Tab[]): Promise<number> {
+  public async getTabGroups(
+    queryInfo: chrome.tabs.QueryInfo,
+  ): Promise<chrome.tabGroups.TabGroup[]> {
     return new Promise((resolve) => {
-      chrome.tabs.group({ tabIds: tabs.map((tab) => tab.id || -1) }, (groupId) => resolve(groupId))
+      chrome.tabGroups.query(queryInfo, (groups) => resolve(groups))
+    })
+  }
+
+  public getTabIds(tabs: chrome.tabs.Tab[]): number[] {
+    return tabs.map((tab) => tab.id || -1)
+  }
+
+  public async newGroup(tabs: chrome.tabs.Tab[]): Promise<number> {
+    return new Promise((resolve) => {
+      chrome.tabs.group({ tabIds: this.getTabIds(tabs) }, (groupId) => resolve(groupId))
+    })
+  }
+
+  public async moveTabsToGroup({ groupId, tabs }: IMoveTabToGroupInput): Promise<number> {
+    return new Promise((resolve) => {
+      chrome.tabs.group({ groupId, tabIds: this.getTabIds(tabs) }, (groupId) => resolve(groupId))
     })
   }
 
@@ -43,10 +65,20 @@ export class TabsManager {
       chrome.tabGroups.update(groupId, { title: tabGroupName }, (group) => resolve(group))
     })
   }
+
   public async addToGroup({ tabGroupName }: IAddToGroupInput): Promise<ITabGroup> {
+    const tabGroups = await this.getTabGroups({ title: tabGroupName })
     const tabs = await this.getTabs(this.getCurrentTabQuery)
-    const groupId = await this.groupTabs(tabs)
-    const group = await this.updateTabGroup({ groupId, tabGroupName })
-    return { id: group.id, title: group.title || '' }
+    if (tabGroups.length === 0) {
+      //tab group does not exist
+      const groupId = await this.newGroup(tabs)
+      const group = await this.updateTabGroup({ groupId, tabGroupName })
+      return { id: group.id, title: group.title || '' }
+    } else {
+      //tab group exists
+      const groupId = tabGroups[0].id
+      await this.moveTabsToGroup({ groupId, tabs })
+      return { id: groupId, title: tabGroupName }
+    }
   }
 }
