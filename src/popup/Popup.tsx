070702fedc0de402bcd15506658fe12391379ad8
Madrigal11 <kurupgokul11@gmail.com>
2023-05-09 12:34:11 +0530

    feat(tab-ui): tag-ui v1.0.0

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -3,45 +3,72 @@ import CmdHandler from '../GroupTabHandlers/CmdHandler/CmdHandler'
 import './Popup.css'
 
 function App() {
-  const [user_input, setUserInput] = useState('')
-  const [logs, setLogs] = useState('')
-  const cmdHandler = new CmdHandler()
+  // const [user_input, setUserInput] = useState('')
+  // const [logs, setLogs] = useState('')
+  // const cmdHandler = new CmdHandler()
+  let tagUI = null
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    const tagInput = document.getElementById('tag-input') as Element
+    const tagContainer = document.querySelector('.tag-container') as Element
 
-  const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = async (event) => {
-    if (event.key === 'Enter') {
-      console.log('Enter key was pressed')
-      const { err: parserErr, cmd, args } = cmdHandler.parseCmd({ user_input })
-      if (parserErr !== '') {
-        setLogs(parserErr)
-        return
+    function createTag(text: string, command: boolean) {
+      if (text === '') return
+      const tag = document.createElement('span')
+      tag.classList.add('tag-item')
+      if (command) {
+        tag.classList.add('tag-command')
       }
-      setLogs(JSON.stringify({ cmd, args }))
-      const { msg, err: handlerErr } = await cmdHandler.handleCmd({ cmd, args })
-      if (handlerErr !== '') {
-        setLogs(handlerErr)
-        return
-      }
-      setLogs(msg)
-      // Perform any additional actions here, such as submitting the form
+      tag.setAttribute('contenteditable', 'false')
+      tag.innerHTML = `
+    ${text.trim()}
+    <button class="tag-remove ml-1">&times;</button>
+  `
+      tag.addEventListener('click', function (event: any) {
+        if (event.target.classList.contains('tag-remove')) {
+          event.target.parentNode.remove()
+        }
+      })
+      tagContainer.insertBefore(tag, tagInput.parentNode)
+      tagInput.textContent = ''
     }
-  }
+
+    function tagInputKeyDown(event: any) {
+      if (event.keyCode === 13) {
+        event.preventDefault()
+        const text = tagInput.textContent || ''
+        const command = tagContainer.children.length <= 1
+        console.log(tagContainer.children)
+        createTag(text, command)
+      }
+    }
+
+    tagInput.addEventListener('keydown', tagInputKeyDown)
+
+    const tagCloseBtns = document.querySelectorAll('.tag-remove')
+    tagCloseBtns.forEach((tagCloseBtn) => {
+      tagCloseBtn.addEventListener('click', function (event: any) {
+        if (event.target.classList.contains('tag-remove')) {
+          event.target.parentNode.remove()
+        }
+      })
+    })
+  }, [])
 
   return (
     <main>
-      <h3>Popup Page!</h3>
+      <h3>GroupTabs</h3>
 
       <h6>v 0.0.0</h6>
-      {/* <p>No tab Group {output}</p> */}
-      <input
-        type="text"
-        className="border rounded py-2 px-3"
-        value={user_input}
-        onChange={(e) => setUserInput(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
-      <p>{logs}</p>
+      <div className="tag-container p-4 border rounded flex flex-wrap items-center">
+        <div id="tag-input-wrapper" className="flex items-center">
+          <div
+            id="tag-input"
+            contentEditable="true"
+            className="border border-gray-300 rounded outline-none text-sm px-2 py-1"
+          ></div>
+        </div>
+      </div>
     </main>
   )
 }
