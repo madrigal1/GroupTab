f0d9adb21c5bbd2f8109704aa2d5ce672463e4e3
Madrigal11 <kurupgokul11@gmail.com>
2023-05-23 12:24:58 +0530

    feat: ui polish v1.0.0

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from 'react'
 import { TagUI } from '../GroupTabHandlers/TagUIManger/tag_ui'
 import './Popup.css'
+import Menu1 from '../assets/menu_1.png'
+import GearIcon from '../assets/gear_img.png'
 
 function App() {
   // const [user_input, setUserInput] = useState('')
@@ -25,10 +27,11 @@ function App() {
   })
 
   return (
-    <main>
-      <h3>GroupTabs</h3>
-
-      <h6>v 0.0.0</h6>
+    <main className="p-2">
+      <header className="flex justify-between item-center">
+        <h2 className="text-black font-medium text-lg">GroupTabs</h2>
+        <h6 className="text-gray-400 font-medium text-custom-md mr-2">v1.0.0</h6>
+      </header>
       <div className="tag-container p-4 border rounded flex flex-wrap items-center">
         <div id="tag-input-wrapper" className="flex items-center">
           <div
@@ -39,6 +42,15 @@ function App() {
           ></div>
         </div>
       </div>
+      <footer className="flex justify-between items-center mt-4 mx-1">
+        <div className="flex items-center">
+          <img className="object-contain mr-2 sm-img" src={Menu1} alt="save button" />
+          <p className="font-semibold">Saves</p>
+        </div>
+        <div>
+          <img src={GearIcon} alt="gear icon" className="md-img" />
+        </div>
+      </footer>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'custom-md': '0.9rem',
+      },
+    },
+    maxWidth: {
+      sm: '0.9rem',
+    },
   },
   plugins: [],
 }
-
