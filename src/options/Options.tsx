daefdee7597fa10dbb8c01c295d65b95440feddf
Madrigal11 <kurupgokul11@gmail.com>
2023-08-13 18:12:07 +0530

    feat: options page v2.0.0

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -17,32 +17,34 @@ function App() {
 
   return (
     <main>
-      <div className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md">
-        <h1 className="text-xl font-semibold mb-4">Group Tab</h1>
-        <section className="space-y-4">
-          <TabList tabs={tabs} />
-        </section>
-      </div>
+      <h1 className="text-xl font-semibold mb-4">Group Tab</h1>
+      <section className="space-y-4">
+        <TabList tabs={tabs} />
+      </section>
     </main>
   )
 }
 
 function TabList({ tabs }: { tabs: chrome.tabs.Tab[] }) {
   return (
-    <div className="p-6">
-      <h1 className="text-xl font-semibold mb-4">Tab List</h1>
+    <div className="p-6 bg-gray-900 text-white">
+      <h1 className="text-2xl font-semibold mb-4">Tab List</h1>
       <div className="overflow-x-auto">
-        <table className="min-w-full border border-gray-300">
+        <table className="min-w-full border border-gray-700">
           <thead>
-            <tr className="bg-gray-100">
+            <tr className="bg-gray-800">
+              <th className="px-4 py-2">Icon</th> {/* New column */}
               <th className="px-4 py-2">Tab Title</th>
               <th className="px-4 py-2">Group ID</th>
             </tr>
           </thead>
-          <tbody className="divide-y divide-gray-300">
+          <tbody className="divide-y divide-gray-700">
             {tabs.map((tab, index) => (
-              <tr key={index} className="bg-white">
-                <td className="px-4 py-2 truncate w-25 max-w-md">{tab.url}</td>
+              <tr key={index} className="bg-gray-900">
+                <td className="px-4 py-2 w-10">
+                  {tab.favIconUrl && <img src={tab.favIconUrl} alt="Favicon" className="w-6 h-6" />}
+                </td>
+                <td className="px-4 py-2 max-w-[0.5rem] truncate text-left">{tab.url}</td>
                 <td className="px-4 py-2">{tab.groupId}</td>
               </tr>
             ))}
